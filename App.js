217daf11e656f99d4017cc6daa2c90157001b6d3
Shanichaun9 <sunnychaun9@gmail.com>
2023-08-22 20:39:10 +0530

    userReducer demo code.

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, TextInput } from 'react-native'
+import React, { useEffect, useReducer, useState } from 'react'
 import { NativeModules } from 'react-native';
 
 const { MyNativeModule} = NativeModules
 
 export default function App() {
-  console.log('nativemodules', MyNativeModule)
-  useEffect(()=>{
-    MyNativeModule.hello('this is the test message',(message) => {
-      console.log(message,'meesage from android native code');
+  // const [name, setName] = useState('')
+  // const [phone, setPhone] = useState('')
+  // const [address, setAddress] = useState('')
+  // const [userName, setUserName] = useState('')
+
+
+  const [state, dispatch] = useReducer((state, action)=>{
+    switch (action.type) {
+      case 'nameAction':
+        return {...state, name:action.payload}
+        case 'phoneAction':
+          return {...state, phone:action.payload}
+          case 'addressAction':
+            return {...state, address:action.payload}
+            case 'userNameAction':
+              return {...state, userName:action.payload}
+      default:
+        return state
+    }
+  },{
+    name:"",
+phone:"",
+address:"",
+userName:""
   });
-console.log('usereffec called')
-  }, [])
+
   return (
     <View>
-      <Text>App</Text>
+     <TextInput style={{borderWidth:1, borderColor:'white'}} onChangeText={(e) => dispatch({type: "nameAction", payload: e})} value={state.name}/>
+     <TextInput style={{borderWidth:1, borderColor:'white'}} onChangeText={(e) => dispatch({type: "phoneAction", payload: e})} value={state.phone}/>
+     <TextInput style={{borderWidth:1, borderColor:'white'}} onChangeText={(e) => dispatch({type: "addressAction", payload: e})} value={state.address}/>
+     <TextInput style={{borderWidth:1, borderColor:'white'}} onChangeText={(e) => dispatch({type: "userNameAction", payload: e})} value={state.userName}/>
+     <Text>name: {state.name}</Text>
+     <Text>phone: {state.phone}</Text>
+     <Text>address: {state.address}</Text>
+     <Text>userName: {state.userName}</Text>
     </View>
   )
 }
