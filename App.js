73bc5a42e3d8045edd9a1f60b30cfc2154453a5f
Shanichaun9 <sunnychaun9@gmail.com>
2023-08-24 20:01:34 +0530

    useMemo example

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import { View, Text, Button } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { NativeModules } from 'react-native';
-import ChildCom from './ChildCom';
-import ChildTwoCom from './ChildTwoCom';
+import { View, Text, Button, TextInput } from 'react-native'
+import React, { useEffect, useMemo, useState } from 'react'
 
 export default function App() {
   const [count, setCount] = useState(0)
   const [theme, setTheme] = useState('dark')
+  const [text, setText] = useState('')
+  const [countArr, setCountArr] = useState([])
+
+  // const getName = useMemo(() =>{
+  //   console.log('generate array called')
+  //   console.log('axio post metho api to retive the data', theme)
+  //   return [count, count + 1 , count + 2]
+  // },[count])
+
+  // const getName = () =>{
+  //   console.log('generate array called')
+  //   console.log('axio post metho api to retive the data', theme)
+  //   return [count, count + 1 , count + 2]
+  // }
+
+  useEffect(()=>{
+    console.log('useEffect called', count)
+    // setCountArr([1,2,3])
+  },[count])
+
+  const setTheSameValue = () =>{
+    // console.log('setThemSameValue', countArr)
+    // setCountArr([1,2,3])
+    setCount(0)
+  }
+
   return (
     <View>
       <Text>Parent component</Text>
+      <Text>count:{count}</Text>
+      <Text>Them:{theme}</Text>
+      {countArr.map((i)=>{
+        // console.log('map function called', i)
+        return <Text>{i}</Text>
+      })}
+      <TextInput onChangeText={(e)=>{setText(e)}} value={text}/>
       <Button title='Add'  onPress={()=> setCount((preState)=> preState + 1)}></Button>
       <Button title='change Theme'  onPress={()=> setTheme((preState)=> preState === 'dark' ? 'light' : 'dark')}></Button>
-      <ChildCom count={count}/>
-      <ChildTwoCom theme={theme} count={count}/>
+      <Button title='change Theme'  onPress={setTheSameValue}></Button>
 
     </View>
   )
